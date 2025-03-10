0a449a134dd1a83f0cf8738733fa2d25069ae5bd
Vishal <rautv2005@gmail.com>
2025-03-10 18:01:02 +0530

    js added

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+const data = document.querySelector("#inputData");
+const contentItems = document.querySelector("#content");
+
+const addItem = () => {
+    if (data.value.trim()) {
+
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `${data.value} <i class="fa fa-times"></i>`;
+
+
+        data.value = "";
+
+
+        listItem.addEventListener("click", () => {
+            listItem.classList.toggle("done");
+        });
+
+
+        listItem.querySelector("i").addEventListener("click", (event) => {
+            event.stopPropagation();
+            listItem.remove();
+        });
+
+        contentItems.prepend(listItem);
+    } else {
+        alert("Please enter a task!");
+    }
+};
+
+
+document.querySelector("#clickbtn").addEventListener("click", addItem);
+
+
+data.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        addItem();
+    }
+});
